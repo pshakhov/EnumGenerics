2c4b87fcf6e1215ad822c18d78b4d3b62b2170c2
stabista <stabista@gmail.com>
2018-10-17 22:19:00 +0300

    2nd commit

--- src/Array.java
+++ src/Array.java
@@ -1,22 +1,52 @@
 import java.util.Arrays;
 
-public class Array{
+ class Array<T>{
 
-    public static void main(String[] args) {
+    private T val;
 
-        Printer printer = new Printer();
-        String[] colors = {"red", "orange", "yellow", "green", "blue"};
-        Integer[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        printer.<String>print(colors);
-        printer.<Integer>print(numbers);
+    public Array(T arg) {
+        val = arg;
+    }
+
+    public String toString() {
+        return "{" + val + "}";
+    }
+
+    public T getValue() {
+        return val;
     }
 }
 
-class Printer{
+ class Printer {
+
+    public static void main(String[] args) {
+        Object[] array = {1, 2, 3, 4, 5};
+
+        for (Object element: array) {
+            System.out.println(element);
+        }
+    }
+    /* public static void main(String[] arg) {
+
+        int size = 10;
+        Object[] arr = new Object[size];
+
+        Printer printer = new Printer();
+        //String[] colors = {"red", "orange", "yellow", "green", "blue"};
+        //Integer[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        //printer.print(arr[1]);
+
+        //printer.<Object>print(arr);
+        //printer.<Integer>print(numbers);
+    } */
+}
+
+/*class Printer{
 
     public <T> void print(T[] items){
         for(T item: items){
-            System.out.println(Arrays.toString(items));
+            System.out.println(Arrays.toString(item));
+
         }
-    }
-}
+    } */
